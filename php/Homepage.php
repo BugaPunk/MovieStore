7ac93f38b112c8b75b00d65fec8b745c1782d363
BugaPunks <punksbuga@outlook.es>
2024-12-05 04:58:44 -0400

    [MODIFY] Estilos css a tailwind a HomePage

--- php/Homepage.php
+++ php/Homepage.php
@@ -2,70 +2,172 @@
     <title>Blu-ray Movies</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../CSS/main.css">
+    <!--link rel="stylesheet" href="../CSS/main.css"-->
     <link rel="stylesheet" href="../CSS/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        rel="stylesheet" />
     <link rel="shortcut icon" type="image/png" href="../img/favicon.png">
 </head>
 
 <body class="m-auto p-auto">
-    <div id="header">
-        <a href="Homepage.php">
-            <img src="../img/Blu-raymovies.png" id="storeName" />
-        </a>
-        <div class="search-container">
-            <form action="SearchResults.php" method="post">
-                <input id="search" type="text" placeholder="Busca Un titulo" name="search">
-                <button type="submit">Busca<i class="fa fa-search"></i></button>
-            </form>
+    <?php
+    session_start();
+    include 'header.php';
+    ?>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap w-full mb-20">
+                <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Categorias📹</h1>
+                    <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+                </div>
+                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon
+                    brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard
+                    of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+            </div>
+            <div class="flex flex-wrap -m-4 mb-2">
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="group relative block h-64 sm:h-80 lg:h-96">
+                        <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+                        <div
+                            class="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+                            <div
+                                class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+                                <svg class="size-10 sm:size-12" xmlns="http://www.w3.org/2000/svg" height="24px"
+                                    viewBox="0 -960 960 960" width="24px" fill="#4b5563">
+                                    <path
+                                        d="M223.89-64.13v-175.72q-36.04-17.48-65.42-45.83-29.38-28.36-50.5-64.72t-32.48-79.48Q64.13-473 64.13-519.94q0-166.26 116.51-271.09 116.5-104.84 299.59-104.84 183.08 0 299.36 104.84Q895.87-686.2 895.87-519.94q0 46.94-11.36 90.06-11.36 43.12-32.48 79.48-21.12 36.36-50.5 64.72-29.38 28.35-65.42 46.22v175.33H223.89ZM337.07-177.3h24v-80h104.56v80h28.74v-80h104.56v80h24V-313q36.24-10.43 65.62-30.41 29.38-19.98 50.38-46.86 21-26.88 32.39-60.16 11.38-33.29 11.38-69.91 0-117.75-83.68-190.05-83.68-72.31-218.89-72.31-135.21 0-219.02 72.31-83.81 72.3-83.81 190.05 0 36.62 11.38 69.91 11.39 33.28 32.39 60.2t50.38 46.86q29.38 19.94 65.62 30.27v135.8Zm60.26-139.59h165.34L480-482.24l-82.67 165.35Zm-57.28-109.63q38.79 0 66.11-27.48 27.32-27.47 27.32-66.05 0-38.79-27.37-66.11t-66.04-27.32q-38.67 0-66.11 27.37-27.44 27.37-27.44 66.04T274-453.96q27.47 27.44 66.05 27.44Zm280 0q38.79 0 66.11-27.48 27.32-27.47 27.32-66.05 0-38.79-27.37-66.11t-66.04-27.32q-38.67 0-66.11 27.37-27.44 27.37-27.44 66.04T554-453.96q27.47 27.44 66.05 27.44ZM480-177.3Z" />
+                                </svg>
+                                <h2 class="mt-4 text-xl font-medium sm:text-2xl">Terror, Horror</h2>
+                            </div>
+
+                            <div
+                                class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+                                <h3 class="mt-4 text-xl font-medium sm:text-2xl">Terror, Horror</h3>
+
+                                <p class="mt-4 text-sm sm:text-base">
+                                    Te moriras del susto, te lo aseguro.
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="group relative block h-64 sm:h-80 lg:h-96">
+                        <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+                        <div
+                            class="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+                            <div
+                                class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" 24px" fill="#4b5563"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h480q33 0 56.5 23.5T720-720v180l160-160v440L720-420v180q0 33-23.5 56.5T640-160H160Zm0-80h480v-480H160v480Zm0 0v-480 480Z"/></svg>
+                                <h2 class="mt-4 text-xl font-medium sm:text-2xl">Accion</h2>
+                            </div>
+
+                            <div
+                                class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+                                <h3 class="mt-4 text-xl font-medium sm:text-2xl">Accion</h3>
+
+                                <p class="mt-4 text-sm sm:text-base">
+                                    Accion a tope.
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="group relative block h-64 sm:h-80 lg:h-96">
+                        <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+                        <div
+                            class="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+                            <div
+                                class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#4b5563"><path d="M480-280q66 0 113-47t47-113H320q0 66 47 113t113 47ZM280-600h160q0-33-23.5-56.5T360-680q-33 0-56.5 23.5T280-600Zm240 0h160q0-33-23.5-56.5T600-680q-33 0-56.5 23.5T520-600ZM480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440v-440h720v440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Zm0-80q116 0 198-82t82-198v-360H200v360q0 116 82 198t198 82Zm0-320Z"/></svg>
+                                <h2 class="mt-4 text-xl font-medium sm:text-2xl">Comedia</h2>
+                            </div>
+
+                            <div
+                                class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+                                <h3 class="mt-4 text-xl font-medium sm:text-2xl">Comedia</h3>
+
+                                <p class="mt-4 text-sm sm:text-base">
+                                    Buen humor.
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="group relative block h-64 sm:h-80 lg:h-96">
+                        <span class="absolute inset-0 border-2 border-dashed border-black"></span>
+                        <div
+                            class="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+                            <div
+                                class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#4b5563"><path d="M440-240q116 0 198-81.5T720-520q0-116-82-198t-198-82q-117 0-198.5 82T160-520q0 117 81.5 198.5T440-240Zm0-280Zm0 160q-83 0-147.5-44.5T200-520q28-70 92.5-115T440-680q82 0 146.5 45T680-520q-29 71-93.5 115.5T440-360Zm0-60q55 0 101-26.5t72-73.5q-26-46-72-73t-101-27q-56 0-102 27t-72 73q26 47 72 73.5T440-420Zm0-40q25 0 42.5-17t17.5-43q0-25-17.5-42.5T440-580q-26 0-43 17.5T380-520q0 26 17 43t43 17Zm0 300q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T80-520q0-74 28.5-139.5t77-114.5q48.5-49 114-77.5T440-880q74 0 139.5 28.5T694-774q49 49 77.5 114.5T800-520q0 64-21 121t-58 104l159 159-57 56-159-158q-47 37-104 57.5T440-160Z"/></svg>
+                                <h2 class="mt-4 text-xl font-medium sm:text-2xl">Thriller</h2>
+                            </div>
+
+                            <div
+                                class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+                                <h3 class="mt-4 text-xl font-medium sm:text-2xl">Terror, Horror</h3>
+
+                                <p class="mt-4 text-sm sm:text-base">
+                                    Te moriras del susto, te lo aseguro.
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="flex flex-wrap -m-4 mt-2">
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                            src="https://dummyimage.com/720x400" alt="content">
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
+                            Distillery hexagon disrupt edison bulbche.</p>
+                    </div>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                            src="https://dummyimage.com/721x401" alt="content">
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
+                            Distillery hexagon disrupt edison bulbche.</p>
+                    </div>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                            src="https://dummyimage.com/722x402" alt="content">
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
+                            Distillery hexagon disrupt edison bulbche.</p>
+                    </div>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                            src="https://dummyimage.com/723x403" alt="content">
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
+                            Distillery hexagon disrupt edison bulbche.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div id="navbar">
-            <ul>
-                <li>
-                    <span class="material-symbols-outlined">
-                        home
-                    </span>
-                    <a href="Homepage.php">Inicio</a>
-                </li>
-                <?php
-                session_start();
-                if (!isset($_SESSION['loggedIn'])) { ?>
-                    <li><a href="Login.php">
-                            <span class="material-symbols-outlined">
-                                person
-                            </span>Iniciar Sesion</a></li>
-                <?php } else { ?>
-                    <li><a href="Checkout.php">
-                            <span class="material-symbols-outlined">
-                                add_shopping_cart
-                            </span>
-                            Carrito</a></li>
-                    <li><a href="logout.php">
-                            <span class="material-symbols-outlined">
-                                logout
-                            </span>
-                            Cerrar</a></li>
-
-                <?php }
-                if (isset($_SESSION['admin'])) {
-                    ?>
-                    <li><a href="modifyMovies.php">
-                            <span class="material-symbols-outlined">
-                                movie_edit
-                            </span>
-                            Modificar Peliculas</a></li>
-                <?php } ?>
-            </ul>
-        </div>
-    </div>
-
-    <div class="banner">
-        <img src="../img/banner.png" class="banner">
-    </div>
-
+    </section>
     <h2 class="homeheader">Categorias</h2>
-
     <div id="featuredCat">
         <div class="gallery">
             <a href="Category.php?category=horror">
